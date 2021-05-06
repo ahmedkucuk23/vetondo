2de7e827f891e823504cc91b17701ea9667200cf
ahmed kucuk <ahmed.kucuk123@gmail.com>
2021-05-06 16:20:11 +0200

    ah,p

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get 'vets', to: 'pages#vets'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :vets
 end
