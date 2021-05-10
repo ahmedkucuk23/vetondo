5b216a3fa72e9490d1ee9aa8e14ca690e9d8c263
ahmed kucuk <ahmed.kucuk123@gmail.com>
2021-05-10 12:16:07 +0200

    CRUD

--- app/controllers/vets_controller.rb
+++ app/controllers/vets_controller.rb
@@ -13,14 +13,32 @@ class VetsController < ApplicationController
   end
 
   def create
+    @vet = Vet.new(vet_params)
+    @vet.save
+    redirect_to vet_path(@vet)
+    # Will raise ActiveModel::ForbiddenAttributesError
   end
 
   def edit
+    @vet = Vet.find(params[:id])
   end
 
   def update
+    @vet = Vet.find(params[:id])
+    @vet.update(vet_params)
+    redirect_to vet_path(@vet)
   end
 
   def destroy
+    @vet = Vet.find(params[:id])
+    @vet.destroy
+    redirect_to vets_path
   end
+
+  private
+
+  def vet_params
+    params.require(:vet).permit(:name, :description, :price)
+  end
+
 end
