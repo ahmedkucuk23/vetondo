b8c454f1fc42edb38a54a07a1afa45cf93b86cde
ahmed kucuk <ahmed.kucuk123@gmail.com>
2021-05-20 15:16:12 +0200

    edit function now

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_11_120003) do
+ActiveRecord::Schema.define(version: 2021_05_20_123508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,9 @@ ActiveRecord::Schema.define(version: 2021_05_11_120003) do
     t.float "latitude"
     t.float "longitude"
     t.string "address"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_vets_on_user_id"
   end
 
+  add_foreign_key "vets", "users"
 end
